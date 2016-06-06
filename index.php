216053536d1c484d10495e3e3bcd4ff9e018c521
Per Nilsson <per@sproutlab.com>
2016-06-06 19:18:49 +0000

    Remove posts logic in index.php
    Hide all the posts!

--- index.php
+++ index.php
@@ -14,51 +14,7 @@
 
             <div id="main" <?php if(get_theme_mod('sp_sidebar_homepage') == true) : ?>class="fullwidth"<?php endif; ?>>
 
-                <?php if (get_theme_mod('sp_home_layout') != 'no_posts') ?>
-
-                    <?php if(get_theme_mod('sp_home_layout') == 'grid' || get_theme_mod('sp_home_layout') == 'full_grid') : ?><ul class="sp-grid"><?php endif; ?>
-
-                    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-                        <?php if(get_theme_mod('sp_home_layout') == 'grid') : ?>
-
-                            <?php get_template_part('content', 'grid'); ?>
-
-                        <?php elseif(get_theme_mod('sp_home_layout') == 'list') : ?>
-
-                            <?php get_template_part('content', 'list'); ?>
-
-                        <?php elseif(get_theme_mod('sp_home_layout') == 'full_list') : ?>
-
-                            <?php if( $wp_query->current_post == 0 && !is_paged() ) : ?>
-                                <?php get_template_part('content'); ?>
-                            <?php else : ?>
-                                <?php get_template_part('content', 'list'); ?>
-                            <?php endif; ?>
-
-                        <?php elseif(get_theme_mod('sp_home_layout') == 'full_grid') : ?>
-
-                            <?php if( $wp_query->current_post == 0 && !is_paged() ) : ?>
-                                <?php get_template_part('content'); ?>
-                            <?php else : ?>
-                                <?php get_template_part('content', 'grid'); ?>
-                            <?php endif; ?>
-
-                        <?php else : ?>
-
-                            <?php get_template_part('content'); ?>
-
-                        <?php endif; ?>
-
-                    <?php endwhile; ?>
-
-                    <?php if(get_theme_mod('sp_home_layout') == 'grid' || get_theme_mod('sp_home_layout') == 'full_grid') : ?></ul><?php endif; ?>
-
-                        <?php solopine_pagination(); ?>
-
-                    <?php endif; /* endif what? */ ?>
-
-                <?php endif; /* endif !no_posts */ ?>
+		<?php /* TODO Actual fix - add "no_posts" "home_layout"  option via https://codex.wordpress.org/Class_Reference/WP_Customize_Manager/add_setting */ ?>
 
             </div>
 
